746230d2e14655a137c1099adf68a04133886a86
litetex <litetex.dev@gmail.com>
2020-05-01 16:46:38 +0200

    Refitted

--- SWAPS/Program.cs
+++ SWAPS/Program.cs
@@ -1,9 +1,12 @@
 ﻿using CommandLine;
 using CoreFrameworkBase.Crash;
 using CoreFrameworkBase.Logging;
+using CoreFrameworkBase.Logging.Initalizer;
+using CoreFrameworkBase.Logging.Initalizer.Impl;
 using SWAPS.CMD;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -12,6 +15,7 @@ namespace SWAPS
    /// <summary>
    /// Main entry point of SWAPS - start programm without auto-starting service
    /// </summary>
+   /// 
    public static class Program
    {
       static void Main(string[] args)
@@ -21,9 +25,11 @@ namespace SWAPS
 
       public static void Run(string[] args)
       {
-         if(!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+         CurrentLoggerInitializer.Set(new DefaultLoggerInitializer(new DefaultLoggerInitializerConfig()));
+
+         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
          {
-            LoggerInitializer.Current.InitLogger(false);
+            InitLog();
             Log.Error("Only Windows is supported");
             Environment.Exit(-1);
             return;
@@ -32,20 +38,34 @@ namespace SWAPS
 #if !DEBUG
          try
          {
-
-            CrashDetector.Current.Init();
+            new CrashDetector()
+            {
+               SupplyLoggerInitalizer = () => {
+                  InitLog(li => li.Config.WriteFile = true);
+                  return CurrentLoggerInitializer.Current;
+               }
+            }.Init();
 #endif
          Parser.Default.ParseArguments<CmdOption>(args)
                   .WithParsed((opt) =>
                   {
-                     LoggerInitializer.Current.InitLogger(opt.LogToFile);
+                     InitLog(li => li.Config.WriteFile = opt.LogToFile);
 
                      var starter = new StartUp(opt);
                      starter.Start();
                   })
                   .WithNotParsed((ex) =>
                   {
-                     LoggerInitializer.Current.InitLogger();
+                     if (ex.All(err =>
+                             new ErrorType[]
+                             {
+                                 ErrorType.HelpRequestedError,
+                                 ErrorType.HelpVerbRequestedError
+                             }.Contains(err.Tag))
+                       )
+                        return;
+
+                     InitLog();
                      foreach (var error in ex)
                         Log.Error($"Failed to parse: {error.Tag}");
                   });
@@ -53,12 +73,15 @@ namespace SWAPS
          }
          catch (Exception ex)
          {
-
-            LoggerInitializer.Current.InitLogger(true);
+            InitLog(li => li.Config.WriteFile = true);
             Log.Fatal(ex);
-
          }
 #endif
+      }
+
+      static void InitLog(Action<DefaultLoggerInitializer> initAction = null)
+      {
+         CurrentLoggerInitializer.InitLogging(il => initAction?.Invoke((DefaultLoggerInitializer)il));
       }
    }
 }

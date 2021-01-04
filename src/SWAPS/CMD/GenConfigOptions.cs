79ffe6193b4d0ad78e3650c324f3c155d04bdbc9
litetex <litetex.dev@gmail.com>
2021-01-04 20:12:47 +0100

    Fixed desc

--- src/SWAPS/CMD/GenConfigOptions.cs
+++ src/SWAPS/CMD/GenConfigOptions.cs
@@ -8,7 +8,7 @@ namespace SWAPS.CMD
    [Verb("genconfig")]
    public class GenConfigOptions : AbstractCmdOptions
    {
-      [Option("path", HelpText = "Generates default config file in mentioned path")]
+      [Option("path", HelpText = "Generates the config file in mentioned path; e.g. dir\\abc.config")]
       public string ConfigGenerationPath { get; set; } = null;
    }
 }
